e43b035aa0eec3570d102f79417189a11cad55c4
Quynh Nguyen <qnguyenx3@gmail.com>
2017-11-13 17:36:53 -0800

    Finally figure out CORS issue with POST request with axios

--- rails-rak/config/routes.rb
+++ rails-rak/config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   post 'organization_token' => 'organization_token#create'
   post 'donor_token' => 'donor_token#create'
-  resources :organizations
+  resources :organizations do
+    resources :projects, only: [:create]
+  end
 
 	resources :projects do
 		resources :events, only: [:show, :update]
